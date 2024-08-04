fcc56b5a27cce944335d647e87f3ba67efd800dc
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2024-08-04 22:34:12 +0530

    Extract the activation at a specified position and pre-commit filrd

--- torchdreams-2/utils.py
+++ torchdreams-2/utils.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import numpy as np
-
 
 def _make_arg_str(arg):
     """
@@ -14,3 +12,21 @@ def _make_arg_str(arg):
     arg = str(arg)
     too_big = len(arg) > 15 or "\n" in arg
     return "..." if too_big else arg
+
+
+def _extract_act_pos(acts, h=None, w=None):
+    """
+    Extract the activation at a specified position.
+
+    Args:
+       acts (torch.Tensor): The input tensor.
+       h (int, optional): The height position. Defaults to None.
+       w (int, optional): The width position. Defaults to None.
+
+    Returns:
+        torch.Tensor: The extracted activation.
+    """
+    shape = acts.shape
+    h = shape[3] // 2 if h is None else h
+    w = shape[4] // 2 if w is None else w
+    return acts[:, :, :, h : h + 1, w : w + 1]
