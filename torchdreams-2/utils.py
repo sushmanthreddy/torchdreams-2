a7b7963ebc141a905af5143d9e2b1c23b8e66bd1
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2024-08-05 02:33:36 +0530

    handls trhe batch tranformations

--- torchdreams-2/utils.py
+++ torchdreams-2/utils.py
@@ -98,3 +98,34 @@ def linconv_reshape(x, ref):
     while x.dim() < ref.dim():
         x = x.unsqueeze(dim=-1)
     return x
+
+
+def batch_transform_handler(transform_func, batch_index=None):
+    """
+    Handle batching for a transformation function.
+
+    Args:
+        transform_func (callable): The transformation function to be applied.
+        batch_index (int, optional): The specific batch index to handle. If None, handle all batches. Defaults to None.
+
+    Returns:
+        callable: A function that applies the transformation to the specified batch or all batches.
+    """
+
+    def apply_transform(name):
+        """
+        Apply the transformation to the specified batch or all batches.
+
+        Args:
+            name (str): The name of the transformation.
+
+        Returns:
+            torch.Tensor: The transformed tensor.
+        """
+        transformed_tensor = transform_func(name)
+        if isinstance(batch_index, int):
+            return transformed_tensor[:, batch_index : batch_index + 1]
+        else:
+            return transformed_tensor
+
+    return apply_transform
